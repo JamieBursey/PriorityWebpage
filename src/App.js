99d21599c6407ca34fa2735e56d1b94b44ebb6a8
Apple <apple@apples-mbp.home>
2024-01-24 16:31:09 -0330

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Home } from "./pages.js";
+import { NavBar } from "./components.js/NavBar.js";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
